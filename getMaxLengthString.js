0ed21cf9ad8caa27735760035de20d3800e4bfdd
= <=>
2022-09-05 15:19:51 +0700

    test1

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,6 +1,21 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  //code here
+  if (!(arrayOfString == null || arrayOfString == undefined))
+  {
+      newArr = []
+    maxArr = []
+    for (elem of arrayOfString) {
+      newArr.push(elem.length)
+    }
+    max = Math.max.apply(Math, newArr)
+    for (elem of arrayOfString) {
+      if (elem.length == max)
+      maxArr.push(elem)
+    }
+    return maxArr
+  }
+
+  return undefined
 }
 module.exports = getMaxLengthString
